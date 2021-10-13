235c2652ba1e7566d9668b77e230f38ba2cbe4c0
Ardak Boranov <ardakb@gmail.com>
2021-10-13 15:30:48 +0600

    удалил переменную тест

--- Movies/MoviesMVVM/MovieScreen/ViewModel/MainViewModel.swift
+++ Movies/MoviesMVVM/MovieScreen/ViewModel/MainViewModel.swift
@@ -5,7 +5,6 @@ import Foundation
 
 protocol MainViewModelProtocol: AnyObject {
     var movies: MoviesPage? { get set }
-    var test: String? { get set }
     var updateViewData: ((MainViewModelProtocol?) -> ())? { get set }
 
     func startFetch(urlString: String)
@@ -18,12 +17,9 @@ final class MVVMViewModel: MainViewModelProtocol {
         }
     }
 
-    var test: String?
-
     var updateViewData: ((MainViewModelProtocol?) -> ())?
 
     func startFetch(urlString: String) {
-        test = String(Int.random(in: 1 ... 1900))
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
             guard let self = self else { return }
@@ -32,7 +28,6 @@ final class MVVMViewModel: MainViewModelProtocol {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 self.movies = try decoder.decode(MoviesPage.self, from: data)
-                //     print(self.movies)
             } catch {
                 print(error.localizedDescription)
             }
